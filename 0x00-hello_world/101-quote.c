07c2bdae752157b357e881314c6f4b366caf3e87
Aphatheology <aphatheology@gmail.com>
2022-03-10 18:48:02 +0100

    Advance task script

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * print text to standard error
+ * Return: 1 if successful
+ */
+int main(void)
+{
+	fprint(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
