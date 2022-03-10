15f36d2119722683883e98e723b58080d05a00db
Aphatheology <aphatheology@gmail.com>
2022-03-10 16:17:41 +0100

    Task 2 to 4 script

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - This function prints out the string
+* "Programming is like building a multilingual puzzle
+* Return: Function returns 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+}
