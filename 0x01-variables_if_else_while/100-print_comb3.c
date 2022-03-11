9618b25d0b4767f1a7c60c8f58627e13e3ac2349
Aphatheology <aphatheology@gmail.com>
2022-03-11 18:54:05 +0100

    Advance task

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * print possible different combinations of two digits
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 48; n < 58; n++)
+	{
+		for (m = 48; m < 58; m++)
+		{
+			putchar(n);
+			putchar(m);
+			if (n != 57 || m != 57)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
