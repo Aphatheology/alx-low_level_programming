fb1204e581db1980cec317109aaf7ad8ed6aea44
Aphatheology <aphatheology@gmail.com>
2022-03-15 15:45:48 +0100

    Task 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - prints alphabet
+ * No parameter needed
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char c = 'a';
+
+	while(c <= 'z')
+	{
+		_putchar('c');
+		c++;
+	}
+	_putchar('\n');
+}
