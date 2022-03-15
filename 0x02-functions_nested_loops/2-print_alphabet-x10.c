78211a158acabc6069081bce3903acf4d1058435
Aphatheology <aphatheology@gmail.com>
2022-03-15 16:04:00 +0100

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet-x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ * No parameter needed
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	int i = 1;
+
+	while (i <= 10)
+	{
+		print_alphabet();
+		i++;
+	}
+}
