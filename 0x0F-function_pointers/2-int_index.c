f14395bd1e27757eb40a18b65b41cea699ed04ca
Abdulkareem Mustapha <aphatheology@gmail.com>
2022-04-13 23:35:58 +0100

    Update task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,26 +1,21 @@
-#include "function_pointers.h"
-
 /**
- * int_index - Searches for an integer in an array.
- * @array: The array.
- * @size: Number of elements in the array.
- * @cmp: Callback function to execute.
- * Return: -1 if no element matches or size is <= 0
- *         Otherwise - The index of the first element for which the
- *         cmp function does not return 0.
+ * int_index - search for int
+ * @array: array to search
+ * @size: size of array
+ * @cmp: iterator function
+ * @Return: index or -1
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array == NULL || cmp == NULL)
-		return (-1);
-
-	for (i = 0; i < size; i++)
+	if (array && cmp && size > 0)
 	{
-		if (cmp(array[i]) != 0)
-			return (i);
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]))
+				return (i);
+		}
 	}
-
 	return (-1);
 }
